adcc5091294989735d26975cf7de2116709f1b3e
Tanguy Siri <tanguy.siri0901@gmail.com>
2024-12-03 11:22:01 +0100

    ajout Exercice 5 et 6 jsobjet

--- exo.js
+++ exo.js
@@ -64,8 +64,8 @@ console.log(cercle.surface());
     this.prix = prix,
     this.description = description;
 }
-    Produit.prototype.infosProduit = function(){ //Ajout de infosProduit exercice 2 deuxième partie
-        return 'Il s\'appel ' + this.nom + ' son prix est de ' + this.prix + '€' + ' et ça déscription c\'est' + this.description; 
+Produit.prototype.infosProduit = function(){ //Ajout de infosProduit exercice 2 deuxième partie
+    return 'Il s\'appel ' + this.nom + ' son prix est de ' + this.prix + '€' + ' et ça déscription c\'est' + this.description; 
 }
 let produitJ = new Produit('Pc', 50,' un outil trop bien'); //Ajout de 3 produits exercice 3 deuxième partie
 let produitS = new Produit('Siri', 1900,' trop cher'); 
@@ -73,3 +73,21 @@ let produitT = new Produit('Tanguy', 1, ' nul');
 console.log(produitJ.infosProduit());
 console.log(produitT.infosProduit());
 console.log(produitS.infosProduit());
+
+//Exercice 4 Création d'une fonction constructeur pour un étudiant
+function Etudiant(nom,niveau,age){
+    this.nom = nom;
+    this.age = age;
+    this.niveau = niveau;
+}
+Etudiant.prototype.presentation = function(){
+    return 'Je suis ' + this.nom + ' j\'étudie en ' + this.niveau + ' et j\'ai ' + this.age + 'ans';
+}
+let etudiant1 = new Etudiant('Imran','5ème année',22);
+let etudiant2 = new Etudiant('Tanguy','5ème année',27);
+let etudiant3 = new Etudiant('Ilan','5ème année',30);
+let etudiant4 = new Etudiant('Tristant','5ème année',20);
+console.log(etudiant1.presentation());
+console.log(etudiant2.presentation());
+console.log(etudiant3.presentation());
+console.log(etudiant4.presentation());
